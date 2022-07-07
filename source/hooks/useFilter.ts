fff06044a8f09c8693a6c24e20456e2c88e5c413
Eko Nur Muhammad Irsyad <ekobokan@MacBook-Air-Eko.local>
2022-07-08 00:37:17 +0700

    hooks

--- source/hooks/useFilter.ts
+++ source/hooks/useFilter.ts
@@ -1,19 +1,77 @@
-import {useCallback, useEffect, useMemo, useState} from 'react';
-import {ITransactionResponse} from '../utilities/transaction-types';
+import {useCallback, useMemo, useState} from 'react';
+import {getTime} from '../utilities/date-transform';
+import {
+  dataSorts,
+  ITransactionResponse,
+  SortList,
+  SortType,
+} from '../utilities/transaction-types';
 import useService from './useService';
 
-function useFilter() {
-  const {data, error} = useService();
-  const [query, setQuery] = useState<string>('');
+/**
+ * Hooks for sort and search
+ */
 
+const useFilter = () => {
+  // get data from service
+  const {data} = useService();
+
+  // state
+  const [query, setQuery] = useState<string>('');
+  const [sorts, setSorts] = useState<SortList>(dataSorts);
+
+  /**
+   * Memoized function to sort and search
+   * based on @query and @sorts
+   * @returns {ITransactionResponse | {}}
+   */
   const transactions = useMemo(() => {
     const isDataExist: boolean = data !== null && Object.keys(data).length > 0;
     const isQueryExist: boolean = query.length > 0;
     if (isDataExist) {
+      let transactionTemps: ITransactionResponse | null = {};
+      const selectedSort = Object.keys(sorts).find(
+        key => sorts[key as keyof typeof SortType].selected,
+      );
+
+      switch (selectedSort) {
+        case SortType.NO_SELECTION:
+          transactionTemps = data;
+          break;
+        case SortType.ASC:
+          transactionTemps = Object.fromEntries(
+            Object.entries(data as ITransactionResponse).sort(([, a], [, b]) =>
+              a.beneficiary_name.localeCompare(b.beneficiary_name),
+            ),
+          );
+          break;
+        case SortType.DESC:
+          transactionTemps = Object.fromEntries(
+            Object.entries(data as ITransactionResponse).sort(([, a], [, b]) =>
+              b.beneficiary_name.localeCompare(a.beneficiary_name),
+            ),
+          );
+          break;
+        case SortType.DATE_ASC:
+          transactionTemps = Object.fromEntries(
+            Object.entries(data as ITransactionResponse).sort(
+              ([, a], [, b]) => getTime(b.created_at) - getTime(a.created_at),
+            ),
+          );
+          break;
+        case SortType.DATE_DESC:
+          transactionTemps = Object.fromEntries(
+            Object.entries(data as ITransactionResponse).sort(
+              ([, a], [, b]) => getTime(a.created_at) - getTime(b.created_at),
+            ),
+          );
+          break;
+      }
+
       if (isQueryExist) {
         const querySearch = query.toLowerCase();
         const filteredByValue = Object.fromEntries(
-          Object.entries(data as ITransactionResponse).filter(
+          Object.entries(transactionTemps as ITransactionResponse).filter(
             ([_, value]) =>
               value.sender_bank.toLowerCase().includes(querySearch) ||
               value.beneficiary_bank.toLowerCase().includes(querySearch) ||
@@ -23,17 +81,35 @@ function useFilter() {
         );
         return filteredByValue;
       }
+
+      return transactionTemps;
     }
-    return data;
-  }, [query, data]);
+    return {};
+  }, [query, data, sorts]);
 
-  console.log(transactions);
-
+  /**
+   * Callback function to set query search
+   */
   const onSearch = useCallback((text: string) => {
     setQuery(text);
   }, []);
 
-  return {data: transactions, onSearch, query};
-}
+  /**
+   * Callback function to set sort
+   */
+  const onSort = useCallback(
+    (type: SortType) => {
+      const newSorts: SortList = {...sorts};
+      Object.keys(newSorts).forEach(key => {
+        newSorts[key as keyof typeof SortType].selected = false;
+      });
+      newSorts[type].selected = true;
+      setSorts(newSorts);
+    },
+    [sorts],
+  );
+
+  return {data: transactions, query, sorts, onSearch, onSort};
+};
 
 export default useFilter;
--- source/hooks/useService.ts
+++ source/hooks/useService.ts
@@ -1,18 +1,23 @@
 import {useEffect, useState} from 'react';
 import {ITransactionResponse} from '../utilities/transaction-types';
 
-function useService() {
+/**
+ * Hooks for get the transaction list fron service
+ */
+const useService = () => {
+  // states
   const [data, setData] = useState<ITransactionResponse | null>(null);
   const [error, setError] = useState(null);
 
-  async function getData(signal: AbortSignal) {
+  // getData and set to state
+  const getData = async (signal: AbortSignal) => {
     return fetch('https://recruitment-test.flip.id/frontend-test', {signal})
       .then(response => response.json())
       .then(json => {
         setData(json);
       })
       .catch(e => setError(e));
-  }
+  };
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -25,6 +30,6 @@ function useService() {
   }, []);
 
   return {data, error};
-}
+};
 
 export default useService;
